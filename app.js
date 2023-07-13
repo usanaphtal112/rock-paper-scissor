3fce8ec6450cb2c50055df670a765d905f518b81
usanaphtal112 <usanaphtal112@gmail.com>
2023-07-14 01:41:59 +0300

    Added loop and reset functionality for the game

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+let userPoints = 0;
+let computerPoints = 0;
+let currentRound = 0;
+
+function startGame() {
+  userPoints = 0;
+  computerPoints = 0;
+  currentRound = 0;
+  document.getElementById("userPoints").textContent = userPoints;
+  document.getElementById("computerPoints").textContent = computerPoints;
+  document.getElementById("roundCount").textContent = currentRound;
+  document.getElementById("result").textContent = "";
+
+  // Enable the click event for the game buttons
+  let gameButtons = document.getElementsByClassName("game-btn");
+  for (let i = 0; i < gameButtons.length; i++) {
+    gameButtons[i].setAttribute(
+      "onclick",
+      `play('${gameButtons[i].dataset.choice}')`
+    );
+  }
+}
+
+function updateRoundCount() {
+  document.getElementById("roundCount").textContent = currentRound;
+}
+
 function play(userChoice) {
   let choices = ["rock", "paper", "scissors"];
-  // console.log(choices.length);
+
   let ranChoice = Math.floor(Math.random() * choices.length);
   let computerChoice = choices[ranChoice];
 
@@ -16,8 +43,41 @@ function play(userChoice) {
   ) {
     document.getElementById("result").innerHTML = "You win!";
     rowElement.classList.add("win-bg");
+    userPoints++;
   } else {
     document.getElementById("result").innerHTML = "You lose!";
     rowElement.classList.add("lose-bg");
+    computerPoints++;
+  }
+
+  currentRound++;
+  document.getElementById("userPoints").textContent = userPoints;
+  document.getElementById("computerPoints").textContent = computerPoints;
+  document.getElementById("roundCount").textContent = currentRound;
+
+  if (currentRound === 5) {
+    endGame();
+  }
+}
+
+function endGame() {
+  let finalResult = "";
+  let rowElement = document.querySelector(".custom-bg-row");
+  rowElement.classList.remove("win-bg", "lose-bg");
+  if (userPoints > computerPoints) {
+    finalResult = "Congratulations! You win!";
+    rowElement.classList.add("win-bg");
+  } else if (userPoints < computerPoints) {
+    finalResult = "You lose! Better luck next time.";
+    rowElement.classList.add("lose-bg");
+  } else {
+    finalResult = "It's a tie! Just no one won";
+  }
+  document.getElementById("result").innerHTML = finalResult;
+
+  // Disable the click event for the game buttons
+  let gameButtons = document.getElementsByClassName("game-btn");
+  for (let i = 0; i < gameButtons.length; i++) {
+    gameButtons[i].removeAttribute("onclick");
   }
 }
